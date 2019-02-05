ad1d40d18804fff222ffeddcb59778fcfe1ed813
Jesse Neumann <jesseneumann@gmail.com>
2019-02-04 21:01:38 -0800

    animations

--- src/Pad.js
+++ src/Pad.js
@@ -1,6 +1,27 @@
 // @flow
 import * as React from 'react'
 
+function getRandomColor(section) {
+  let results = [];
+  let counter = 3;
+  let min;
+  let max;
+  if(section === 'inside') {
+    min = 128
+    max = 256
+  } else {
+    min = 0;
+    max = 128;
+  }
+
+  while (counter--) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    results.push(Math.floor(Math.random() * (max - min)) + min);
+  }
+  return  `rgb(${results})`
+}
+
 class Pad extends React.Component {
   componentDidMount () {
     document.addEventListener('keydown', (e)=> {
@@ -17,12 +38,25 @@ class Pad extends React.Component {
     this.props.updateDisplay(this.props.name)
   }
 
+  handlePointerEnter = (e) => {
+    let color = [];
+    color.push(getRandomColor('inside'))
+    color.push(getRandomColor('outside'))
+    e.target.style.background = `radial-gradient(${color})`
+  }
+
+  handlePointerLeave = (e) => {
+    e.target.style = { 'animation' : '3s fade' };
+  }
+
   render () {
     return (
       <div
         className="drum-pad"
         id={this.props.name}
         onClick={this.playSample}
+        onPointerEnter={this.handlePointerEnter}
+        onPointerLeave={this.handlePointerLeave}
       >
         <audio
           className="clip"
