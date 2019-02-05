cc8b28ce607d17c86e0c8aa1ef29cbbc5c658f43
Jesse Neumann <jesseneumann@gmail.com>
2019-02-04 17:10:30 -0800

    chose 9 samples, removed the rest

--- src/DrumPad.js
+++ src/DrumPad.js
@@ -1,23 +1,37 @@
 // @flow
 import * as React from 'react'
 import Pad from './Pad'
-import Sounds from './Sounds.js'
+import sound1 from './assets/808 Bass A Overdrive .wav'
+import sound2 from './assets/Clap 004.wav'
+import sound3 from './assets/HiHat Closed 002 Ozone.wav'
+import sound4 from './assets/HiHat Open 001.wav'
+import sound5 from './assets/Kick 002 Knock.wav'
+import sound6 from './assets/Kick 007 Noisy.wav'
+import sound7 from './assets/Loading a Gun.wav'
+import sound8 from './assets/Orchestra Hit 001 Revved.wav'
+import sound9 from './assets/Snare 003.wav'
 
-
+const sounds = [
+  { name: '808 Bass', wav: sound1 },
+  { name: 'Clap', wav: sound2 },
+  { name: 'HiHat Closed', wav: sound3 },
+  { name: 'HiHat Open', wav: sound4 },
+  { name: 'Kick Knock', wav: sound5 },
+  { name: 'Kick Noisy', wav: sound6 },
+  { name: 'Loading Gun', wav: sound7 },
+  { name: 'Orchestra Hit', wav: sound8 },
+  { name: 'Snare', wav: sound9 },
+]
 
 class DrumPad extends React.Component {
-  state = {
-    soundsArray: [ 3, 41, 31, 20, 24, 28, 32, 36, 40 ]
-  }
-
   render () {
     const keyNames = [ 'Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C']
-    const pads = this.state.soundsArray.map((soundIndex, i) => (
+    const pads = sounds.map((sound, i) => (
       <Pad
         key={i}
         id={keyNames[i]}
-        src={Sounds[soundIndex].sample}
-        name={Sounds[soundIndex].name}
+        src={sound.wav}
+        name={sound.name}
         updateDisplay={this.props.updateDisplay}
       />
     ))
